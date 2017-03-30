cfbe465f31d54596b6ce4f0079d52ca91d9c79c6
Peter Puszkiewicz <piotrp@microsoft.com>
2017-03-30 07:43:20 -0700

    Very simple sample

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace src
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            return 0;
+        }
+    }
+}
